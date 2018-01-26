20a215bcbc6877fd80a8c4a9ef409fa29023c3da
Valery Jacobs <valeryjacobs@hotmail.com>
2018-01-26 13:58:12 +0100

    A lot of changes

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,12 @@
 var Hub = artifacts.require("Hub");
-var Policy = artifacts.require("Policy");
-var User = artifacts.require("User");
+//var Policy = artifacts.require("Policy");
+//var User = artifacts.require("User");
 
 var Adoption = artifacts.require("Adoption");
 
 module.exports = function(deployer) {
   deployer.deploy(Adoption);
   deployer.deploy(Hub);
-  deployer.deploy(Policy);
-  deployer.deploy(User);
+  //deployer.deploy(Policy);
+  //deployer.deploy(User);
 };
